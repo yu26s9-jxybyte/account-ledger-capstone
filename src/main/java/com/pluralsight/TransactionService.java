7a1f23f8966d7389dacf7af10e038330d942ab1b
jxybyte <ramosjayln@gmail.com>
2026-04-27 14:22:35 -0400

    created the save method for transactions.

--- src/main/java/com/pluralsight/TransactionService.java
+++ src/main/java/com/pluralsight/TransactionService.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public class TransactionService {
+    public static ArrayList<Transaction> loadTransactions(){
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"));
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                String[] parts = line.split("\\|");
+
+                String date = parts[0];
+                String time = parts[1];
+                String description = parts[2];
+                String shop = parts[3];
+                double amount = Double.parseDouble(parts[4]);
+
+                Transaction t = new Transaction(date, time, description, shop, amount);
+                transactions.add(t);
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Could not load all transactions.");
+        }
+        return transactions;
+
+    }
+    public static void saveTransaction(Transaction t){
+        try{
+            FileWriter writer = new FileWriter("transactions.csv", true );
+            writer.write(t.toCSV() + " ");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Could not save the transactions.");
+        }
+    }
 }
